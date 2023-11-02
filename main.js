76feff16cb8d356f564cba5b71445ece7d6d94b3
labib3013 <alfarabyhd30@gmail.com>
2023-11-02 10:12:20 +0700

    feat(): temp

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const halo = "Assalamualaikum";
 const halo2 = "Waalaikumsalam";
 
+const testing = [1, 2, 0, 3, 4];
 for (let i = 0; i < halo.length; i++) {
+  console.log(halo[i]);
   console.log(halo[i]);
 }
 
